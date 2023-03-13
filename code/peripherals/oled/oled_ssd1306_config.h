83d7ec724bd0a5c427b0953a26fec77a1d803631
daniel.zhang <daniel.zhang@fulicare.com>
2023-03-13 17:45:24 +0800

    适配oled仓库

---
+++ code/peripherals/oled/oled_ssd1306_config.h
@@ -0,0 +1,17 @@
+#ifndef _OLED_SSD1306_CONFIG__
+#define _OLED_SSD1306_CONFIG__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define OLCD_DC_PIN                     16
+#define OLCD_CS_PIN                     15
+#define OLCD_REST_PIN                   17
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

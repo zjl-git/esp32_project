e464f3ee8a179db49fe77c34fde15e35da3bbf25
daniel.zhang <daniel.zhang@fulicare.com>
2023-03-28 18:40:55 +0800

    增加minilzo测试

--- code/apps/apps_init.c
+++ code/apps/apps_init.c
@@ -3,15 +3,52 @@
 #include "app_home_screen_idle_activity.h"
 
 #include "ux_manager.h"
+#include "minilzo.h"
+
 #define LOG_TAG        "apps"
 
 /******************************************************************************/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#define IN_LEN          4 * 1024
+#define OUT_LEN         IN_LEN + IN_LEN / 16 + 64 + 3
+static uint8_t in_buf[IN_LEN];
+static uint8_t out_buf[OUT_LEN];
+static uint8_t work_buf[IN_LEN + 64];
+
 TaskHandle_t g_test_task_handle;
 static void test_task_handle(void *arg)
 {
     uint8_t a = 2;
+
+    uint8_t re;
+    uint32_t in_len = IN_LEN;
+    uint32_t out_len = 0, new_len = 0;
+
+    uint32_t i = 0;
+    for (i = 0; i < IN_LEN; i++) {
+        in_buf[i] = i + 1;
+    }
+    // memset(in_buf, 0x00, in_len);
+
+    re = lzo1x_1_compress(in_buf, in_len, out_buf, &out_len, work_buf);
+    if (re == LZO_E_OK) {
+        APPS_LOG_D(LOG_TAG, "minilzo compressed %d btyes into %d bytes", in_len, out_len);
+    } else {
+        APPS_LOG_E(LOG_TAG, "minilzo internal error - compression failed:%d", re);
+        return ;
+    }
+
+    new_len = in_len;
+    re = lzo1x_decompress(out_buf, out_len, in_buf, &new_len, NULL);
+    if (re == LZO_E_OK) {
+        APPS_LOG_D(LOG_TAG, "minilzo decompressed %d btyes into %d bytes", out_len, new_len);
+    } else {
+        APPS_LOG_E(LOG_TAG, "minilzo internal error - decompression failed:%d", re);
+        return ;
+    }
+
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(5000));
         ux_send_event(false, UX_ACTIVITY_EVENT_PRIORITY_LOWSET, a, a+1, NULL, 0, NULL, 0);
@@ -32,11 +69,6 @@ static void apps_configs_module_init(void)
     ux_set_pre_proc_func(app_preproc_activity);
     ux_start_activity(NULL, app_home_screen_idle_activity, UX_ACTIVITY_PRIORITY_IDLE_BACKGROUND, NULL, 0);
     ux_start();
-}
-
-static void task_def_create(void)
-{
-
 }
 
 
@@ -69,7 +101,9 @@ void apps_init(void)
 
     apps_configs_module_init();
 
-    task_def_create();
+    if (lzo_init() != LZO_E_OK) {
+        APPS_LOG_E(LOG_TAG, "minilzo init fail!!!");
+    }
 
     APPS_LOG_D(LOG_TAG, "apps init complete");
 
